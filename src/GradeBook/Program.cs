826ed42d2a827d79231d03b02210e8a8f2ae5142
Keith Heffington <dransiktolkien@yahoo.com>
2019-08-09 06:58:50 -0500

    Looping through Arrays

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,14 +9,20 @@ namespace GradeBook
             var x = 35.1;
             var y = 25.7;
             var result = x + y;
-            var numbers = new double[3];
-            numbers[0] = 12.7;
+            var numbers = new[] {12.7, 17.5, 15.9,}; // array
+            /* numbers[0] = 12.7;
             numbers[1] = 17.5;
-            numbers[2] = 15.9;
-            var arrayresult = numbers[0] + numbers[1] + numbers[2];
+            numbers[2] = 15.9; */
+            // var arrayresult = numbers[0] + numbers[1] + numbers[2];
+            var arrayresult = 0.0;
+            foreach (var number in numbers)
+            {
+              arrayresult += number;               
+            }
+            Console.WriteLine($"{arrayresult}");  
 
-            Console.WriteLine($"{numbers[0]} + {numbers[1]} + {numbers[2]} = {arrayresult}");
-            Console.WriteLine($"{x} + {y} = {result}");
+            //Console.WriteLine($"{numbers[0]} + {numbers[1]} + {numbers[2]} = {arrayresult}");
+            //Console.WriteLine($"{x} + {y} = {result}");
 
             if(args.Length > 0)
             {
