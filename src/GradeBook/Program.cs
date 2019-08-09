e1088a61524810b476fd3bbb63e3a1568206106c
Keith Heffington <dransiktolkien@yahoo.com>
2019-08-09 07:20:33 -0500

    Computing the Average

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,21 +7,33 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var x = 35.1;
-            var y = 25.7;
-            var result = x + y;
-            var numbers = new[] {12.7, 17.5, 15.9,}; // array
+            //var x = 35.1; Test Data
+            //var y = 25.7; Test Data
+            //var result = x + y; Test Data
+            //var numbers = new[] {12.7, 17.5, 15.9,}; // array Test Data
+            var grades = new List<double>() {12.7, 17.5, 15.9,};
+            grades.Add(56.1);
+        
             /* numbers[0] = 12.7;
             numbers[1] = 17.5;
             numbers[2] = 15.9; */
             // var arrayresult = numbers[0] + numbers[1] + numbers[2];
-            var arrayresult = 0.0;
+            var gradesresult = 0.0;
+            foreach (var number in grades)
+            {
+                gradesresult += number;
+            }
+            gradesresult /= grades.Count;       
+            System.Console.WriteLine($"The average grade is {gradesresult:N1}");
+
+            /* Foreach loop testing
+                var arrayresult = 0.0;
             foreach (var number in numbers)
             {
               arrayresult += number;               
             }
             Console.WriteLine($"{arrayresult}");  
-
+             */   
             //Console.WriteLine($"{numbers[0]} + {numbers[1]} + {numbers[2]} = {arrayresult}");
             //Console.WriteLine($"{x} + {y} = {result}");
 
