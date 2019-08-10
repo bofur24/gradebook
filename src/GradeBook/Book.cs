b72f9cf1c3eb38fd68da51d45a8c0be9769b78ec
Keith Heffington <dransiktolkien@yahoo.com>
2019-08-10 08:53:51 -0500

    Created Statistics in Book

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -14,6 +15,24 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;      
+            System.Console.WriteLine($"The lowest grade is {lowGrade}");
+            System.Console.WriteLine($"The Highest grade is {highGrade}"); 
+            System.Console.WriteLine($"The average grade is {result:N1}");
         }
 
         private List<double> grades;
